af2bc522bbbb2cf103e1379b3b51462ecbe8e7b9
PORT-G-AURELIEN\Aurélien <curti.aurelien@free.fr>
2017-04-10 10:44:22 +0200

    Mise en place des classes et méthodes de base pour manipuler l'objet BusinessCard en base de donnée

---
+++ app/src/main/java/com/example/aurlien/businesscard/BusinessCardDAO.java
@@ -0,0 +1,48 @@
+package com.example.aurlien.businesscard;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Aurélien on 10/04/2017.
+ */
+
+public class BusinessCardDAO extends DAOBase{
+    public static final String TABLE_NAME = "businesscard";
+    public static final String ID = "id";
+    public static final String NOM = "nom";
+    public static final String TEL = "telephone";
+
+    public static final String TABLE_CREATE = "CREATE TABLE " + TABLE_NAME + " (" + ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + NOM + " TEXT, " + TEL + " TEXT);";
+
+    public static final String TABLE_DROP =  "DROP TABLE IF EXISTS " + TABLE_NAME + ";";
+
+    public void ajouter(BusinessCard card) {
+        ContentValues value = new ContentValues();
+        value.put(BusinessCardDAO.NOM, card.getNom());
+        value.put(BusinessCardDAO.TEL, card.getTelephone());
+        mDb.insert(BusinessCardDAO.TABLE_NAME, null, value);
+    }
+
+    public void supprimer(long id) {
+        mDb.delete(TABLE_NAME, ID + " = ?", new String[] {String.valueOf(id)});
+    }
+
+    public void modifier(BusinessCard card) {
+        ContentValues value = new ContentValues();
+        value.put(NOM, card.getNom());
+        value.put(TEL, card.getTelephone());
+        mDb.update(TABLE_NAME, value, ID  + " = ?", new String[] {String.valueOf(card.getId())});
+    }
+
+    public BusinessCard selectionner(long id) {
+        Cursor c = mDb.rawQuery("select " + NOM + "," + TEL + " from " + TABLE_NAME + " where "+ID+ " = ?", new String[]{String.valueOf(id)});
+        c.moveToFirst();
+        long id_res = c.getLong(0);
+        String nom_res = c.getString(1);
+        String tel_res = c.getString(2);
+        return new BusinessCard(id_res, nom_res, tel_res);
+    }
+
+}
---
+++ app/src/main/java/com/example/aurlien/businesscard/DatabaseHandler.java
@@ -0,0 +1,39 @@
+package com.example.aurlien.businesscard;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Aurélien on 10/04/2017.
+ */
+
+public class DatabaseHandler extends SQLiteOpenHelper{
+    public static final String BC_ID = "id";
+    public static final String BC_NOM = "nom";
+    public static final String BC_TEL = "telephone";
+
+    public static final String BC_TABLE_NAME = "Businesscard";
+    public static final String BC_TABLE_CREATE =
+            "CREATE TABLE " + BC_TABLE_NAME + " (" +
+                    BC_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    BC_NOM + " TEXT, " +
+                    BC_TEL + " TEXT);";
+
+    public static final String METIER_TABLE_DROP = "DROP TABLE IF EXISTS " + BC_TABLE_NAME + ";";
+
+    public DatabaseHandler(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(BC_TABLE_CREATE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(METIER_TABLE_DROP);
+        onCreate(db);
+    }
+}
